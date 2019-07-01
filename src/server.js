3072ea059cd40061aabd28f3ae8ffae6cd25fb11
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-06-30 17:30:58 -0700

    setup heroku first deploy

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const app = require("./app");
 const http = require("http");
 const server = http.createServer(app);
+const port = process.env.PORT || 3000;
 
-server.listen(3000);
+server.listen(port);
 
 server.on('listening', () => {
-  console.log('server is listening on port 3000');
+  console.log(`server is listening on port ${port}`);
 });
