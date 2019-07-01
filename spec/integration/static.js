df3eb9dd97592017added825297a22ddb6c76ab4
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-06-30 17:21:40 -0700

    create head and navbar partials

--- spec/integration/static.js
+++ spec/integration/static.js
@@ -5,11 +5,23 @@ const server = require('../../src/server');
 describe('routes : static', () => {
 
   describe('GET /', () => {
-    it ('should render a landing page containing "News Your Own Adventure"', () => {
+    it('should render a landing page containing "News Your Own Adventure"', () => {
       request.get(base, (err, res, body) => {
         expect(err).toBeNull();
         expect(res.statusCode).toBe(200);
         expect(body).toContain('News Your Own Adventure');
+      });
+    });
+
+    it('should display navigation bar', () => {
+      request.get(base, (err, res, body) => {
+        expect(body).toContain('Sign In', 'Sign Up');
+      });
+    });
+
+    it('should include head from patial', () => {
+      request.get(base, (err, res, body) => {
+        expect(body).toContain('<link rel="stylesheet"');
       });
     });
   });
--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require('path');
 const staticRoutes = require('../src/routes/static/index');
 
-app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, 'assets')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(staticRoutes);
