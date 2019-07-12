92a425b0c1e6f4b4027e5770760f419924c1e925
sqwisha <40513088+sqwisha@users.noreply.github.com>
2019-07-12 16:20:42 -0700

    fix auth helper redirect

--- src/auth/helpers.js
+++ src/auth/helpers.js
@@ -4,7 +4,7 @@ module.exports = {
   ensureAuthenticated(req, res, next) {
     if (!req.user) {
       req.flash('notice', 'You must be logged in to do that');
-      res.redirect(req.header('Referer'));
+      res.redirect(req.header('Referer') || '/');
     } else {
       next();
     }
